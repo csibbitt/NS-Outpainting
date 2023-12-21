7e3f2932900b5c0a49d1289a8496924b320a1330
Chris Sibbitt <csibbitt@gmail.com>
2023-12-21 16:00:18 -0500

    more like that

--- train.sh
+++ train.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-if [ ! -f dataset/trainset.tfr ]; then
+CHECKPOINT=24963
+
+export CUDA_HOME=/usr/local/cuda
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=/usr/local/cuda
+
+if [ ! -f tf_dataset_new/trainset.tfr ]; then
   wget "https://drive.google.com/uc?export=download&id=1XcL0guFyqhLns_HgkFBEKEpbUHQ1dA6U&confirm=yes" -O tf_scenery.zip
   unzip tf_scenery.zip
 fi
 
-export CUDA_HOME=/usr/local/cuda
-export XLA_FLAGS=--xla_gpu_cuda_data_dir=/usr/local/cuda
-python train_model.py  --trainset-path ./dataset/trainset.tfr --testset-path ./dataset/testset.tfr
+python train_model.py\
+  --trainset-path ./tf_dataset_new/trainset.tfr \
+  --testset-path ./tf_dataset_new/testset.tfr \
+  --log-path ./logs/ \
+  --checkpoint-path "./drive/MyDrive/v2_NS-Outpainting/logs/1215/2/models/-${CHECKPOINT}" \
+  --resume-step "${CHECKPOINT}"
