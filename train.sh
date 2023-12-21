8dff61d136ad0e1ad6af4ef69b1647ae40e2b88a
Chris Sibbitt <csibbitt@gmail.com>
2023-12-21 15:46:44 -0500

    train for nothere

--- train.sh
+++ train.sh
@@ -1,2 +1,10 @@
-export XLA_FLAGS=--xla_gpu_cuda_data_dir=/usr/lib/cuda
+#!/bin/sh
+
+if [ ! -f dataset/trainset.tfr ]; then
+  wget "https://drive.google.com/uc?export=download&id=1XcL0guFyqhLns_HgkFBEKEpbUHQ1dA6U&confirm=yes" -O tf_scenery.zip
+  unzip tf_scenery.zip
+fi
+
+export CUDA_HOME=/usr/local/cuda
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=/usr/local/cuda
 python train_model.py  --trainset-path ./dataset/trainset.tfr --testset-path ./dataset/testset.tfr
