8632571daddf6c6155125e0015cd9b34b0e6796d
Chris Sibbitt <csibbitt@gmail.com>
2024-01-03 23:23:16 -0500

    Model weight parity with original v1 code!
    * Total size of variables: 127795458
    * Total bytes of variables: 511181832
    * Adding shc previously missed

---
+++ modeling/shc.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+import tensorflow_addons as tfa
+
+import modeling.relu as mr
+
+# Skip Horizontal Connection
+class Shc(tf.keras.layers.Layer):
+
+  def build_regularizer(self):
+    return tf.keras.regularizers.L2(self.decay)
+
+  def build_conv1(self):
+    return tf.keras.layers.Conv2D(self.channels / 2, 1, strides=(1,1), activation=tf.nn.relu,
+                  padding='same', use_bias=False,
+                  kernel_regularizer=tf.compat.v1.keras.utils.get_or_create_layer("shc_conv1" + str(self.channels) + "_reg", self.build_regularizer),
+    )
+
+  def build_conv2(self):
+    return tf.keras.layers.Conv2D(self.channels / 2, 3, strides=(1,1), activation=tf.nn.relu,
+                  padding='same', use_bias=False,
+                  kernel_regularizer=tf.compat.v1.keras.utils.get_or_create_layer("shc_conv2" + str(self.channels) + "_reg", self.build_regularizer)
+    )
+
+  def build_conv3(self):
+    return tf.keras.layers.Conv2D(self.channels, 1, strides=(1,1), activation=None,
+                  padding='same', use_bias=False,
+                  kernel_regularizer=tf.compat.v1.keras.utils.get_or_create_layer("shc_conv3" + str(self.channels) + "_reg", self.build_regularizer)
+    )
+
+  def __init__(self, decay, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.decay = decay
+
+  def call(self, x, shortcut, channels):
+    self.channels = channels
+
+    x = tf.compat.v1.keras.utils.get_or_create_layer("shc_conv1_" + str(channels), self.build_conv1)(x)
+    x = tf.compat.v1.keras.utils.get_or_create_layer("shc_conv1_" + str(channels) + "_in", tfa.layers.InstanceNormalization)(x)
+
+    x = tf.compat.v1.keras.utils.get_or_create_layer("shc_conv2_" + str(channels), self.build_conv2)(x)
+    x = tf.compat.v1.keras.utils.get_or_create_layer("shc_conv2_" + str(channels) + "_in", tfa.layers.InstanceNormalization)(x)
+
+    x = tf.compat.v1.keras.utils.get_or_create_layer("shc_conv3_" + str(channels), self.build_conv3)(x)
+    x = tf.compat.v1.keras.utils.get_or_create_layer("shc_conv3_" + str(channels) + "_in", tfa.layers.InstanceNormalization)(x)
+
+    return tf.add(shortcut, x)
